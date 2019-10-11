f3f648375bd626683a689ae0737371507e473293
Cody Barr <cody.barr@gmail.com>
2019-10-11 14:45:50 -0400

    Updated to use the significantly more efficient @nosweat/scrabble module.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-const scrabble = require('scrabble')
+const scrabble = require('@nosweat/scrabble')
 
 module.exports = (req, res) => {
-	const { body, query } = req
+	const { query } = req
 
 	if (!query.letters) {
 		return res.status(400).send('You need to provide a letters query.')
 	}
 
 	try {
-		let answers = scrabble(query.letters)
+		const answers = scrabble.find(query.letters)
 		return res.status(200).json(answers)
 	} catch (err) {
 		return res.status(400).send('Something went wrong. Try again.')
